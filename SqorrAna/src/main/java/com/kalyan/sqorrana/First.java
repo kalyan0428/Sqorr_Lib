5f6b0791b06442b25f5bef31d11d3577d3ed48ea
unknown <kalyan0428@gmail.com>
2020-12-16 16:52:48 +0530

    first commit

---
+++ SqorrAna/src/main/java/com/kalyan/sqorrana/First.java
@@ -0,0 +1,13 @@
+package com.kalyan.sqorrana;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class First {
+
+    public static void myInfo(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
